68b3b3a611498d245c7aad166d6dec07de573d85
terehova <aterehova@awg.ru>
2026-03-16 12:25:41 +0300

    дз к уроку 3 - Объекты, массивы и функции

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,14 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const parts = path.split('.');
 
+  return function (obj) {
+    return parts.reduce((acc, key) => { 
+      if (acc && Object.hasOwn(acc, key)) {
+        return acc[key];
+      }
+      return undefined;
+    }, obj);
+  };
 }
--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,24 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (!string || size <= 0) {
+    return '';
+  }
+  if (size === undefined) {
+    return string;
+  }
+  let count = 0;
+  return string.split('').reduce((acc, char) => {
+    if (char === acc[acc.length - 1]) {
+      count++;
+    } else {
+      count = 1;
+    }
 
+    if (count <= size) {
+      acc.push(char);
+    }
+    
+    return acc;
+  }, []).join('');
 }
